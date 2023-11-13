272c483f93bc3ff8c8bd8081715ddfa400e9b20e
odalys quiñonez <135558827+Odalysquinonez@users.noreply.github.com>
2023-11-13 14:47:07 -0500

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from personas.models import Persona
 
+       
 # Create your views here.
 def Bienvenido (request):
-    return render(request,"bienvenido.html")
+    #mensajes ={'msg1': 'valor mensaje 1', 'msg2': 'valor mensaje 2'}
+    no_personas = Persona.objects.count()
+    personas = Persona.objects.all()
+    #return render(request,"bienvenido.html",{'msg1': 'valor mensaje 1', 'msg2': 'valor mensaje 2'} )
+    return render(request,"bienvenido.html",{'no_personas':no_personas,'personas': personas})
+
+
+
+
+
+
 
 def Contacto (request):
     return HttpResponse("Mi nombre es Odalys y mi telefono es 0967605159")
